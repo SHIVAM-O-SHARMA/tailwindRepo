6ba8ee2e4edbf97a95f5d9f9973cc4821aa00656
shivam <[shivamsharmas01230@gmail.com]>
2025-06-21 16:39:45 +0530

    added tailwind basics

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import { Otp } from './comoponents/otp' 
+import { SideBarClass1 } from './comoponents/sidebar'
+
+// // otp box code written below
+
+// function App(){
+//   return (
+// <div className='h-screen' style={{backgroundColor: "#002b5a"}}>
+//   <br /><br /><br /><br />
+//   <Otp/>
+//   <button className='transform-all duration-1000 bg-black rounded-2xl hover:bg-green-200 p-4 hover:p-8'>hi there</button>
+// </div>
+//   )
+// }
+
+// export default App 
+
+
+// sidebar code written below
+
+function App(){
+  return (
+    <div>
+      <SideBarClass1 />
+    </div>
+  )
+}
+
+export default App
