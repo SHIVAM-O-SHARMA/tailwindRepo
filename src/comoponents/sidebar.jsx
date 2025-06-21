1500e4ec6cbbf70159394e168b50462b7f13d5d2
shivam <[shivamsharmas01230@gmail.com]>
2025-06-21 16:44:31 +0530

    changed width of sidebar

--- src/comoponents/sidebar.jsx
+++ src/comoponents/sidebar.jsx
@@ -1,7 +1,7 @@
 export function SideBarClass1(){
     return (
   <div className="flex" >
-    <div className="transition-all ease-in-out duration-700 bg-amber-200 md:w-64 w-0 h-screen">sidebar</div>
+    <div className="transition-all ease-in-out duration-700 bg-amber-200 md:w-64 w-1 h-screen">sidebar</div>
     <div className="bg-amber-700 w-full h-screen">content</div>
   </div>
 )} 
